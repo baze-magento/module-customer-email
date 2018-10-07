9a334e553f6bc46531198a1f3bc9b52150029173
Emma Ewert <EmmaEwert@users.noreply.github.com>
2018-10-08 00:48:51 +0200

    Add initial code

---
+++ Console/Command/CustomerEmailWelcomeCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Baze\CustomerEmail\Console\Command;
+
+use Magento\Framework\App\State;
+use Magento\Customer\Model\CustomerFactory;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CustomerEmailWelcomeCommand extends Command
+{
+	protected $appState;
+	protected $customerFactory;
+
+	public function __construct(State $appState, CustomerFactory $customerFactory)
+	{
+		$this->appState = $appState;
+		$this->customerFactory = $customerFactory;
+		parent::__construct();
+	}
+
+	protected function configure()
+	{
+		$this->setName('customer:email:welcome')
+			->setDescription('Send welcome email to all users in the specified website');
+		$this->addOption('website', null, InputOption::VALUE_REQUIRED, 'Send welcome emails to all users in this website');
+		parent::configure();
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$website = $input->getOption('website');
+		$output->writeln("<info>Sending welcome emails to all users in $website…</info>");
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# module-customer-email
+# Baze_CustomerEmail
+
+## Architecture
+
+Modules that require theme changes <- Core theme -> Child "skin" themes
+
+**Standalone modules** (This Repo) -> (no dependencies)
+
+## Installation
+
+```bash
+# as root
+systemctl stop cron
+```
+```bash
+# as user
+magento maintenance:enable
+composer config repositories.module-customer-email vcs https://github.com/bazedk/module-customer-email
+composer require baze/module-customer-email:^1.0
+magento module:enable Baze_CustomerEmail
+magento setup:upgrade
+magento setup:di:compile
+magento setup:static-content:deploy en_US da_DK
+magento cache:clean
+magento cache:flush
+magento maintenance:disable
+```
+```bash
+# as root
+systemctl restart php7.1-fpm
+systemctl start cron
+```
+
+## Usage
+
+```bash
+# as user
+magento customer:email:send --website=code
+```
