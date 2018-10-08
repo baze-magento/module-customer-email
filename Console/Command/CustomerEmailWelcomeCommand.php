53450ffec335ef82d61faa71fe265ad8492305ea
Emma Ewert <EmmaEwert@users.noreply.github.com>
2018-10-08 03:21:41 +0200

    Filter initial customers list

--- Console/Command/CustomerEmailWelcomeCommand.php
+++ Console/Command/CustomerEmailWelcomeCommand.php
@@ -3,6 +3,7 @@
 namespace Baze\CustomerEmail\Console\Command;
 
 use Magento\Framework\App\State;
+use Magento\Framework\App\ObjectManager;
 use Magento\Customer\Model\CustomerFactory;
 
 use Symfony\Component\Console\Command\Command;
@@ -14,6 +15,10 @@ class CustomerEmailWelcomeCommand extends Command
 {
 	protected $appState;
 	protected $customerFactory;
+	protected $emailNotification;
+	protected $storeManager;
+
+	const NEW_ACCOUNT_EMAIL_REGISTERED_NO_PASSWORD = 'customer/create_account/email_no_password_template';
 
 	public function __construct(State $appState, CustomerFactory $customerFactory)
 	{
@@ -25,7 +30,7 @@ class CustomerEmailWelcomeCommand extends Command
 	protected function configure()
 	{
 		$this->setName('customer:email:welcome')
-			->setDescription('Send welcome email to all users in the specified website');
+			->setDescription('Send welcome email to all users in a specified website');
 		$this->addArgument('website', InputArgument::REQUIRED, 'Send welcome emails to all users in this website');
 		parent::configure();
 	}
@@ -33,6 +38,31 @@ class CustomerEmailWelcomeCommand extends Command
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$website = $input->getArgument('website');
-		$output->writeln("<info>Sending welcome emails to all users in ${website}…</info>");
+		$websiteId = getStoreManager()->getWebsites(false, true)[$website]->getId(); // $withDefault, $codeKey
+		$output->writeln("<info>Sending welcome emails to all users in '${website}', ID ${websiteId}…</info>");
+
+		$customers = $this->customerFactory->create()->getCollection();
+		$customers->addFieldToFilter('website_id', $websiteId);
+		foreach ($customers as $customer) {
+			$storeId = $customer->getStoreId();
+			output->writeln("newAccount($customer, $templateType, $redirectUrl, $storeId)");
+			// $this->getEmailNotification()->newAccount($customer, $templateType, $redirectUrl, $customer->getStoreId());
+		}
+	}
+
+	private function getEmailNotification()
+	{
+		if (!($this->emailNotification instanceof EmailNotificationInterface)) {
+			$this->emailNotification = ObjectManager::getInstance()->get(EmailNotificationInterface::class);
+		}
+		return $this->emailNotification;
+	}
+
+	private function getStoreManager()
+	{
+		if (!($this->storeManager instanceof StoreManagerInterface)) {
+			$this->storeManager = ObjectManager::getInstance()->get(StoreManagerInterface::class);
+		}
+		return $this->storeManager;
 	}
 }
